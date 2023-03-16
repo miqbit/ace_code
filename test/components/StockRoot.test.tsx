781caa8b3c39ab16a4d2a69763b2eb3b07460e9b
miqbit <92051724+miqbit@users.noreply.github.com>
2023-03-16 15:09:14 +0000

    Various Fixes

--- test/components/StockRoot.test.tsx
+++ test/components/StockRoot.test.tsx
@@ -36,3 +36,64 @@ const StockExtractor = ({
 };
 
 describe('StockRoot context testing', () => {
+	it('should pass clone of initialValues into context', () => {
+		const initialValues = {
+			hello: 'asdf',
+			parent: {
+				child: {
+					value: 'test',
+				},
+			},
+			array: [
+				0,
+				{
+					nested: 'value',
+				},
+			],
+		};
+
+		let realValues: object | undefined = undefined;
+
+		act(() => {
+			render(
+				<StockRoot initialValues={initialValues}>
+					<div>
+						<h2>Test component</h2>
+						<p>other component</p>
+						<StockExtractor logStock={(stock) => (realValues = stock.getValues())} />
+					</div>
+				</StockRoot>,
+				container,
+			);
+		});
+
+		expect(realValues).toStrictEqual(initialValues);
+	});
+});
+
+describe('StockRoot performance testing', () => {
+	it('should never rerender StockRoot', () => {
+		const RerenderCounter: React.FC = jest.fn(() => <div />);
+
+		act(() => {
+			let stock: Stock<any> | undefined = undefined;
+
+			render(
+				<StockRoot initialValues={{}}>
+					<RerenderCounter />
+					<StockExtractor logStock={(_stock) => (stock = _stock)} />
+				</StockRoot>,
+				container,
+			);
+
+			if (stock) {
+				const _stock = stock as any;
+				_stock.setValue(createPxth(['hello']), 'asdf');
+				_stock.setValue(createPxth(['nested', 'value']), 3);
+				_stock.setValue(createPxth(['asdfasd', 'asdf', 'asdf']), 'asdf');
+			}
+		});
+
+		expect(RerenderCounter).toBeCalledTimes(1);
+	});
+});
---
+++ test/hooks/useAllStockValues.test.tsx
@@ -0,0 +1,82 @@
+import React from 'react';
+import { act, renderHook } from '@testing-library/react-hooks';
+import { createPxth } from 'pxth';
+
+import { Stock, StockContext, useAllStockValues, useStock } from '../../src';
+
+const initialValues = {
+	hello: 'test',
+	parent: {
+		child: 'value',
+	},
+	array: [
+		1,
+		{
+			value: 'second',
+		},
+	],
+};
+
+let stock: Stock<typeof initialValues>;
+
+const wrapper: React.FC = ({ children }) => (
+	<StockContext.Provider value={stock as unknown as Stock<object>}>{children}</StockContext.Provider>
+);
+
+const renderUseStockValues = () =>
+	renderHook(() => useAllStockValues(stock), {
+		wrapper,
+	});
+
+beforeEach(() => {
+	const { result } = renderHook(() => useStock({ initialValues }), { wrapper });
+	stock = result.current;
+});
+
+describe('Testing "useStockValues" with context stock', () => {
+	it('Should return new values after update using setValues', async () => {
+		const { result, waitForNextUpdate } = renderUseStockValues();
+
+		expect(result.current).toStrictEqual(initialValues);
+
+		const promise = act(async () => {
+			await waitForNextUpdate({ timeout: 1000 });
+		});
+
+		const newValue = {
+			hello: 'changed',
+			parent: {
+				child: 'changed',
+			},
+			array: [
+				42,
+				{
+					value: 'changed',
+				},
+			],
+		};
+
+		stock.setValues(newValue);
+
+		await promise;
+
+		expect(result.current).toStrictEqual(newValue);
+	});
+	it('Should return new values after update using setValue', async () => {
+		const { result, waitForNextUpdate } = renderUseStockValues();
+
+		expect(result.current).toStrictEqual(initialValues);
+
+		const promise = act(async () => {
+			await waitForNextUpdate({ timeout: 1000 });
+		});
+
+		const newValue = [1, 42];
+
+		stock.setValue(createPxth(['array']), newValue);
+
+		await promise;
+
+		expect(result.current).toStrictEqual({
+			hello: 'test',
+			par
